970edb8c20fbbbed373a68a2293d6c39be448ea7
Vik1ang <vik1ang@outlook.com>
2022-03-23 17:13:11 +0800

    q1

---
+++ Leetcode/Q1/Solution.cs
@@ -0,0 +1,20 @@
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var dic = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (dic.ContainsKey(target - nums[i]))
+            {
+                return new int[] { dic[target - nums[i]], i };
+            }
+            else
+            {
+                dic[nums[i]] = i;
+            }
+        }
+
+        return new int[] { 0 , 0};
+    }
+}
